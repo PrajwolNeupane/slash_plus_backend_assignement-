f5530117729e93369220e4012254097c3d239165
PrajwolNeupane <prajwolneupane68@gmail.com>
2024-06-07 23:37:41 +0545

    added mongo db connection

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import dbConfig from './config/dbConfig.js';
+
+const port = 3000;
+const app = express();
+app.use(
+    cors({
+        origin: [
+            process.env.CLIENT_URL, ,
+            process.env.LIVE_CLIENT_URL,
+        ],
+        credentials: true,
+    })
+);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Working Fine"
+    })
+});
+
+app.listen(port, async () => {
+    console.log("---------------------------");
+    console.log(`Server is listening at ${port}`);
+    await dbConfig;
+});
