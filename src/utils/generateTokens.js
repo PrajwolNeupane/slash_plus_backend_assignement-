b660d6729702f01d2eb3a27b5c8c293e454fc152
PrajwolNeupane <prajwolneupane68@gmail.com>
2024-06-08 02:29:53 +0545

    refact:refactore some changes during client integration

--- src/utils/generateTokens.js
+++ src/utils/generateTokens.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 // Generate Access and Refresh Tokens
 const generateTokens = (payload) => {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN_SECRET, {
-        expiresIn: "30m",
+        expiresIn: "32m",
     });
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_TOKEN_SECRET, {
         expiresIn: "1d",
